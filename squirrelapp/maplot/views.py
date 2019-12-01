69e9c32d47db57e00203b35c23dfb1c7586986e1
diefigueroa96 <diefigueroa96@gmail.com>
2019-12-01 19:40:46 +0000

    first change to views.py, test

--- squirrelapp/maplot/views.py
+++ squirrelapp/maplot/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+
+def sightings(request):
+    return HttpResponse("Hello, world. this is sightings page.")
+
+def update_delete(request):
+    return HttpResponse("Hello, world. this is update/delete page.")
+
+def add(request):
+    return HttpResponse("Hello, world. this is add page.")
 # Create your views here.
