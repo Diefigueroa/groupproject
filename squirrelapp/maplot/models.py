a2dc790263c63f4b5e24acdaa51da0ddd2fb1dd3
diefigueroa96 <diefigueroa96@gmail.com>
2019-12-01 21:22:06 +0000

    I modified models.py first time

--- squirrelapp/maplot/models.py
+++ squirrelapp/maplot/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from adaptor.model import CsvModel
+
+class squirrel(CsvModel):
+    Latitude = FloatField()
+    Longitude = FloatField()
+    Unique_Squirrel_ID = IntegerField()
+    Shift = CharField()
+    Date = DateField()
+    Age = CharField()
+    Primary_Fur_Color = CharField()
+    Location = CharField()
+    Specific_Location = CharField()
+    Running = FloatField()
+    Chasing = FloatField()
+    Climbing = FloatField()
+    Eating = FloatField()
+    Foraging = FloatField()
+    Other_Activities = CharField()
+    Kuks = FloatField()
+    Quaas = FloatField()
+    Moans = FloatField()
+    Tail_flags = FloatField()
+    Tail_twitches = FloatField()
+    Approaches = FloatField()
+    Indifferent = FloatField()
+    Runs_from = FloatField()
+
+    class Meta:
+        delimiter = ";"
 
 # Create your models here.
