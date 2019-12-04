561863efa5bf0691e65d39eea0a2468ceac69c4e
diefigueroa96 <diefigueroa96@gmail.com>
2019-12-04 02:55:47 +0000

    new changes to models.py

--- squirrelapp/maplot/models.py
+++ squirrelapp/maplot/models.py
@@ -2,32 +2,31 @@ from django.db import models
 
 from adaptor.model import CsvModel
 
-class squirrel(CsvModel):
-    Latitude = FloatField()
-    Longitude = FloatField()
-    Unique_Squirrel_ID = IntegerField()
-    Shift = CharField()
-    Date = DateField()
-    Age = CharField()
-    Primary_Fur_Color = CharField()
-    Location = CharField()
-    Specific_Location = CharField()
-    Running = FloatField()
-    Chasing = FloatField()
-    Climbing = FloatField()
-    Eating = FloatField()
-    Foraging = FloatField()
-    Other_Activities = CharField()
-    Kuks = FloatField()
-    Quaas = FloatField()
-    Moans = FloatField()
-    Tail_flags = FloatField()
-    Tail_twitches = FloatField()
-    Approaches = FloatField()
-    Indifferent = FloatField()
-    Runs_from = FloatField()
+class squirrel(models.Model):
+    Latitude = models.FloatField(default=0)
+    Longitude = models.FloatField(default=0)
+    Unique_Squirrel_ID = models.IntegerField(default=0)
+    Shift = models.CharField(max_length=200)
+    Date = models.DateField('date published')
+    Age = models.CharField(max_length=200)
+    Primary_Fur_Color = models.CharField(max_length=200)
+    Location = models.CharField(max_length=200)
+    Specific_Location = models.CharField(max_length=200)
+    Running = models.FloatField(default=0)
+    Chasing = models.FloatField(default=0)
+    Climbing = models.FloatField(default=0)
+    Eating = models.FloatField(default=0)
+    Foraging = models.FloatField(default=0)
+    Other_Activities = models.CharField(max_length=200)
+    Kuks = models.FloatField(default=0)
+    Quaas = models.FloatField(default=0)
+    Moans = models.FloatField(default=0)
+    Tail_flags = models.FloatField(default=0)
+    Tail_twitches = models.FloatField(default=0)
+    Approaches = models.FloatField(default=0)
+    Indifferent = models.FloatField(default=0)
+    Runs_from = models.FloatField(default=0)
 
-    class Meta:
-        delimiter = ";"
+    
 
 # Create your models here.
