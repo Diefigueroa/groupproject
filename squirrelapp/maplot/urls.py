35a38010b235f333c136b721ab36408ec75d8eb7
Ava Yang <ty2418@instance-1.c.tools1-255501.internal>
2019-12-07 03:42:12 +0000

    url

--- squirrelapp/maplot/urls.py
+++ squirrelapp/maplot/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
 
+    path('', views.index, name='index'),     
     path('map/', views.map, name='map'),
     path('sightings/', views.sightings, name='sightings'),
     path('sightings/<int:Unique_Squirrel_ID>/', views.edit, name='edit'),
