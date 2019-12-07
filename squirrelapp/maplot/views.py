d08cdea0bf4497ad333a12c1d1d4aa97f7d8dfe7
diefigueroa96 <diefigueroa96@gmail.com>
2019-12-07 00:52:35 +0000

    working on maps markers part1

--- squirrelapp/maplot/views.py
+++ squirrelapp/maplot/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
+from .models import squirrel
 
 def map(request):
     template = loader.get_template('maplot/map.html')
