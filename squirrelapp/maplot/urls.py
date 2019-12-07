844c3994ea6deb5ac042ba3597f9e05152580969
Ava Yang <ty2418@instance-1.c.tools1-255501.internal>
2019-12-07 19:39:44 +0000

    urls

--- squirrelapp/maplot/urls.py
+++ squirrelapp/maplot/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),     
     path('map/', views.map, name='map'),
     path('sightings/stats/', views.squirrel_stats,name='stats'),
-    path('sightings/<int:Unique_Squirrel_ID>/', views.edit, name='edit'),
+    path('sightings/<str:Unique_Squirrel_ID>/', views.edit, name='edit'),
     path('sightings/add/', views.add, name='add'),
     
 ]
