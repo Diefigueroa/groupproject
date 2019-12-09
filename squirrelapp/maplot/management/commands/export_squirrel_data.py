d255e8ec61300d4bd04312e3f1090ad1f248d92d
Ava Yang <ty2418@instance-1.c.tools1-255501.internal>
2019-12-09 21:00:16 +0000

    export

--- squirrelapp/maplot/management/commands/export_squirrel_data.py
+++ squirrelapp/maplot/management/commands/export_squirrel_data.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from maplot.models import squirrel
-import requests, csv, re, sys
+import csv, re, sys
 from dateutil import parser
 from datetime import date
 
@@ -12,39 +12,37 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         path = kwargs['path']
-        
-        with open(path, mode='wb') as csvfile:
+        with open(path, mode='w') as csvfile:
             writer = csv.writer(csvfile)
             squirrels = squirrel.objects.all()
-            writer.writerow['X','Y','Unique Squirrel ID',
-                    'Shift','Date','Age','Primary Fur Color','Location','Specific Location','Running','Chasing','Climbing','Eating','Foraging','Other Activities','Kuks','Quaas','Moans','Tail flags','Tail twitches','Approaches','Indifferent','Runs from']
+            writer.writerow(['X','Y','Unique Squirrel ID','Shift','Date','Age','Primary Fur Color','Location','Specific Location','Running','Chasing','Climbing','Eating','Foraging','Other Activities','Kuks','Quaas','Moans','Tail flags','Tail twitches','Approaches','Indifferent','Runs from'])
 
                         
 
-            for squirrel in squirrels:
+            for row in squirrels:
                     writer.writerow([
-                    squirrel.Latitude,Longitude,
-                    squirrel.Unique_Squirrel_ID,
-                    squirrel.Shift,
-                    squirrel.Date ,
-                    squirrel.Age ,
-                    squirrel.Primary_Fur_Color ,
-                    squirrel.Location ,
-                    squirrel.Specific_Location ,
-                    squirrel.Running ,
-                    squirrel.Chasing ,
-                    squirrel.Climbing ,
-                    squirrel.Eating ,
-                    squirrel.Foraging ,
-                    squirrel.Other_Activities ,
-                    squirrel.Kuks ,
-                    squirrel.Quaas ,
-                    squirrel.Moans ,
-                    squirrel.Tail_Flags ,
-                    squirrel.Tail_Twitches ,
-                    squirrel.Approaches ,
-                    squirrel.Indifferent ,
-                    squirrel.Runs_From ,
+                    row.latitude,row.longitude,
+                    row.Unique_Squirrel_ID,
+                    row.Shift,
+                    row.Date ,
+                    row.Age ,
+                    row.Primary_Fur_Color ,
+                    row.Location ,
+                    row.Specific_Location ,
+                    row.Running ,
+                    row.Chasing ,
+                    row.Climbing ,
+                    row.Eating ,
+                    row.Foraging ,
+                    row.Other_Activities ,
+                    row.Kuks ,
+                    row.Quaas ,
+                    row.Moans ,
+                    row.Tail_flags ,
+                    row.Tail_twitches ,
+                    row.Approaches ,
+                    row.Indifferent ,
+                    row.Runs_from ,
                 ])
         csvfile.close() 
         
